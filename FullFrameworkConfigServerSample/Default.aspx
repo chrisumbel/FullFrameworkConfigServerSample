6f3784f06239c7cba7eb72f4c227f4761f1dfdee
Chris Umbel <chris@chrisumbel.com>
2018-06-14 09:25:20 -0700

    initial imoprt

---
+++ FullFrameworkConfigServerSample/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FullFrameworkConfigServerSample._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>IP Address:</h1> <%= this.IpAddress %>
+    </div>
+
+</asp:Content>
---
+++ FullFrameworkConfigServerSample/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FullFrameworkConfigServerSample
+{
+    public partial class _Default : Page
+    {
+        public string IpAddress { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ApplicationConfig.Configuration.Reload();
+            this.IpAddress = ApplicationConfig.Configuration["customer:0:ipAddress"];
+        }
+        
+    }
+}
